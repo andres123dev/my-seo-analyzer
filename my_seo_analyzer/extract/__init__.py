6c9f4185de2dc54838b7f616f8bffcf3d58a6b96
Andrés Fernández Burón <193231832+andres123dev@users.noreply.github.com>
2025-01-22 21:33:16 +0100

    Update __init__.py

--- my_seo_analyzer/extract/__init__.py
+++ my_seo_analyzer/extract/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ =        "Andrés Fernández Burón"
 __name__ =          "extract"
 __description__ =   "Module to get a HTML webpage & analize his SEO"
-__copyright__ =     "Copyright 2022-2025, Andrés Fernández Burón"
+__author__ =        "Andrés Fernández Burón"
+__copyright__ =     "Copyright 2022-2025"
 __license__ =       "All rights reserved"
 __date__ =          "15-05-2022"
 __status__ =        "Development"
@@ -16,6 +16,3 @@ __all__ = [
     'http_request',
     'seo_analysis'
 ]
-
-from .http_request import *
-from .seo_analysis import *
