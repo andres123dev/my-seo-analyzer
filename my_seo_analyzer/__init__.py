fd131817846a62356f2f4dd3a241f3510708dc04
Andrés Fernández Burón <193231832+andres123dev@users.noreply.github.com>
2025-01-22 21:29:36 +0100

    Update __init__.py

--- my_seo_analyzer/__init__.py
+++ my_seo_analyzer/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ =        "Andrés Fernández Burón"
 __name__ =          "my_seo_analyzer"
 __description__ =   "Console app to request an URL, analize the SEO and generate a report."
+__author__ =        "Andrés Fernández Burón"
 __copyright__ =     "Copyright 2022-2025, Andrés Fernández Burón"
 __license__ =       "All rights reserved"
 __date__ =          "15-05-2022"
@@ -15,9 +15,14 @@ __email__ =         "https://github.com/andres123dev/my-seo-analyzer/new/choose"
 __all__ = [
     'utils',
     'extract',
-    'report'
+    'report',
 ]
 
-from .utils import *
-from .extract import *
-from .report import *
+import my_seo_analyzer.utils.text
+import my_seo_analyzer.utils.user_interface
+import my_seo_analyzer.utils.filesys
+import my_seo_analyzer.extract.http_request
+import my_seo_analyzer.extract.seo_analysis
+import my_seo_analyzer.report.styles
+import my_seo_analyzer.report.template
+import my_seo_analyzer.report.output
